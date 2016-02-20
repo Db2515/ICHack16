3096d3d09de96cd6540281f472cab7474de3f978
db2515 <db2515@ic.ac.uk>
2016-02-20 23:22:18 +0000

    User: Added an adress for profile pictures and a set method.

--- UniPlanner/app/src/main/java/com/dbpb/uniplanner/com/dbpb/uniplanner/groups/User.java
+++ UniPlanner/app/src/main/java/com/dbpb/uniplanner/com/dbpb/uniplanner/groups/User.java
@@ -3,6 +3,8 @@ package com.dbpb.uniplanner.com.dbpb.uniplanner.groups;
 public class User {
 
     private String username;
+    private String profilePictureAddress = "https://lh6.ggpht.com/3GquZbIDgiQc-wQlb9ui3Nt" +
+                                           "j3CxuRLKwMUop6miqlv91Pod6AuPjSigl_jBT7d99ig=w300";
 
     public User(String username){
         this.username = username;
@@ -10,5 +12,9 @@ public class User {
 
     public String getUsername(){
         return username;
+    }
+
+    public void setProfilePicture(String address){
+       profilePictureAddress = address;
     }
 }
